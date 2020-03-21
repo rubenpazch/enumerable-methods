3b436bd630139b1c01166646f80055e90c46b68c
rubenpazch <rubenpazchuspe@outlook.com>
2020-03-21 16:59:11 -0500

    adding each methods file

---
+++ each_methods.rb
@@ -0,0 +1,21 @@
+module Enumerable
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    i = 0
+    while i < length
+      yield (self[i])
+      i += 1
+    end
+  end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    i = 0
+    while i < length
+      yield self[i], i
+      i += 1
+    end
+	end
+end
