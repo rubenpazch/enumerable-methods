e567345097a2006ec4c19476360468adb2f93030
rubenpazch <rubenpazchuspe@outlook.com>
2020-03-22 23:51:59 -0500

    adding testcases to the main file

--- main.rb
+++ main.rb
@@ -8,3 +8,15 @@ def multiply_els(numbers)
 end
 
 puts multiply_els([1, 2, 3, 4])
+puts %w[ant bear cat].my_all? { |word| word.length >= 3 }
+puts %w[ant bear cat].my_any? { |word| word.length >= 3 }
+puts %w[ant bear cat].my_none? { |word| word.length == 5 }
+
+ary = [1, 2, 4, 2]
+ary.my_count(&:even?)
+
+puts [1, 2, 3].my_inject(4) { |prod, n| prod * n } # should provide me 24 but got 6
+puts (1...4).my_inject(:+) # should return 6
+puts (1...4).my_inject(1, :+) # should return 7
+puts (1..4).my_map { |i| i * i }
+puts (1..4).my_map_proc { |i| i * i }
